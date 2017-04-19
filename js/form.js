df6ba1e94ac5955496b01f857b47a8c048eb91f4
Aleksandr Sannikov <alexander.v.sannikov@gmail.com>
2017-04-19 16:09:37 +0300

    Update

--- js/form.js
+++ js/form.js
@@ -2,23 +2,25 @@
 
 var notice = document.querySelector('.notice');
 var noticeForm = notice.querySelector('.notice__form');
-var typeOfProperty = noticeForm.getElementById('type');
-var priceOfProperty = noticeForm.getElementById('price');
-var roomNumberOfProperty = noticeForm.getElementById('room_number');
-var capacityOfProperty = noticeForm.getElementById('capacity');
-var checkinOfProperty = noticeForm.getElementById('time');
-var checkoutOfProperty = noticeForm.getElementById('timeout');
+var typeOfProperty = noticeForm.querySelector('#type');
+var priceOfProperty = noticeForm.querySelector('#price');
+var roomNumberOfProperty = noticeForm.querySelector('#room_number');
+var capacityOfProperty = noticeForm.querySelector('#capacity');
+var checkinOfProperty = noticeForm.querySelector('#time');
+var checkoutOfProperty = noticeForm.querySelector('#timeout');
 
 validateForm();
 
 function validateForm() {
+  var checkoutTime = checkoutOfProperty.querySelector('')
   checkinOfProperty.addEventListener('change', function (evt) {
 
   });
 }
 
 function isCheckedOption(select) {
-  var checkedOption = select.querySelector('option[selected]');
+  var checkedOption = select.querySelector('option[selected]').value;
+  console.log(checkedOption);
 
   return checkedOption;
 }
