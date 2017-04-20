93e659fa67b9d6d2b1d9ab3dd607025ffe1f1c20
Aleksandr Sannikov <alexander.v.sannikov@gmail.com>
2017-04-20 11:24:14 +0300

    Module 4 Task 2

--- js/form.js
+++ js/form.js
@@ -12,15 +12,56 @@ var checkoutOfProperty = noticeForm.querySelector('#timeout');
 validateForm();
 
 function validateForm() {
-  var checkoutTime = checkoutOfProperty.querySelector('')
-  checkinOfProperty.addEventListener('change', function (evt) {
+  checkinOfProperty.addEventListener('change', onCheckinChange);
+  checkoutOfProperty.addEventListener('change', onCheckoutChange);
+  roomNumberOfProperty.addEventListener('change', onRoomsNumberChange);
+  typeOfProperty.addEventListener('change', onTypeChange);
+  capacityOfProperty.addEventListener('change', onCapacityChange);
 
+  noticeForm.addEventListener('submit', function () {
+    noticeForm.reset();
   });
-}
 
-function isCheckedOption(select) {
-  var checkedOption = select.querySelector('option[selected]').value;
-  console.log(checkedOption);
+  function onCheckinChange(evt) {
+    checkoutOfProperty.value = evt.target.value;
+  }
 
-  return checkedOption;
+  function onCheckoutChange(evt) {
+    checkinOfProperty.value = evt.target.value;
+  }
+
+  function onTypeChange(evt) {
+    switch (evt.target.value) {
+      case 'flat':
+        priceOfProperty.value = 1000;
+        priceOfProperty.min = 1000;
+        break;
+      case 'hut':
+        priceOfProperty.value = 0;
+        priceOfProperty.min = 0;
+        break;
+      default:
+        priceOfProperty.value = 10000;
+        priceOfProperty.min = 10000;
+        break;
+    }
+  }
+
+  function onRoomsNumberChange(evt) {
+    switch (evt.target.value) {
+      case '1':
+        capacityOfProperty.value = 0;
+        break;
+      default:
+        capacityOfProperty.value = 3;
+    }
+  }
+
+  function onCapacityChange(evt) {
+    switch (evt.target.value) {
+      case '3':
+        roomNumberOfProperty.value = 2;
+        break;
+    }
+  }
 }
