87a6765fffd5b55a43851ebc0dcbdf9bb15d282d
alexis-sanches <alexander.v.sannikov@gmail.com>
2017-04-27 07:32:39 +0300

    Работа с сетью

---
+++ js/load.js
@@ -0,0 +1,63 @@
+// load.js
+'use strict';
+
+window.load = (function () {
+  var onError = function (error) {
+    var errorContainer = document.createElement('div');
+    var errorMessage = document.createElement('p');
+
+    errorContainer.classList.add('error');
+    errorMessage.textContent(error);
+    errorContainer.appendChild(errorMessage);
+    document.querySelector('body').appendChild(errorContainer);
+  };
+
+  var load = function (url, onLoad) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      var error;
+
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          console.log(xhr.response);
+          break;
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+        default:
+          error = 'Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText;
+          break;
+      }
+      if (error) {
+        onError(error);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', url);
+    xhr.send();
+  };
+
+  return {
+    getData: load
+  };
+})();
--- js/map.js
+++ js/map.js
@@ -2,52 +2,54 @@
 'use strict';
 
 (function () {
-  var NUMBER_OF_PROPERTIES = 8;
+  window.load.getData('https://intensive-javascript-server-kjgvxfepjl.now.sh/keksobooking/data', onLoad);
 
-  var offerDialog = document.getElementById('offer-dialog');
-  var properties = window.data.createProperties(NUMBER_OF_PROPERTIES);
-  var pins = window.render.createPins(properties);
-  var dialogClose = offerDialog.querySelector('.dialog__close');
-  var pinContainer = document.querySelector('.tokyo__pin-map');
+  function onLoad(response) {
+    var offerDialog = document.getElementById('offer-dialog');
+    var dialogClose = offerDialog.querySelector('.dialog__close');
+    var pinContainer = document.querySelector('.tokyo__pin-map');
+    var properties = response;
+    var pins = window.render.createPins(properties);
 
-  pins.forEach(function (it, i) {
-    window.showCard(it, offerDialog, properties[i], openDialog);
-  });
-
-  document.addEventListener('keydown', function (evt) {
-    if (window.utils.isEscCode(evt.keyCode) && !offerDialog.classList.contains('hidden')) {
-      closeDialog(evt);
-    }
-  });
-
-  dialogClose.addEventListener('click', function (evt) {
-    closeDialog(evt);
-  });
-
-  dialogClose.addEventListener('keydown', function (evt) {
-    if (window.utils.isEnterCode(evt.keyCode)) {
-      closeDialog(evt);
-    }
-  });
-
-  window.render.renderPins(pinContainer, pins);
-
-  function openDialog(pin, property) {
-    deactivatePins();
-    pin.classList.add('pin--active');
-    offerDialog.classList.remove('hidden');
-    window.card.renderPropertyElement(property);
-  }
-
-  function closeDialog(evt) {
-    evt.preventDefault();
-    deactivatePins();
-    offerDialog.classList.add('hidden');
-  }
-
-  function deactivatePins() {
-    pins.forEach(function (pin) {
-      pin.classList.remove('pin--active');
+    pins.forEach(function (it, i) {
+      window.showCard(it, offerDialog, properties[i], openDialog);
     });
+
+    document.addEventListener('keydown', function (evt) {
+      if (window.utils.isEscCode(evt.keyCode) && !offerDialog.classList.contains('hidden')) {
+        closeDialog(evt);
+      }
+    });
+
+    dialogClose.addEventListener('click', function (evt) {
+      closeDialog(evt);
+    });
+
+    dialogClose.addEventListener('keydown', function (evt) {
+      if (window.utils.isEnterCode(evt.keyCode)) {
+        closeDialog(evt);
+      }
+    });
+
+    window.render.renderPins(pinContainer, pins);
+
+    function openDialog(pin, property) {
+      deactivatePins();
+      pin.classList.add('pin--active');
+      offerDialog.classList.remove('hidden');
+      window.card.renderPropertyElement(property);
+    }
+
+    function closeDialog(evt) {
+      evt.preventDefault();
+      deactivatePins();
+      offerDialog.classList.add('hidden');
+    }
+
+    function deactivatePins() {
+      pins.forEach(function (pin) {
+        pin.classList.remove('pin--active');
+      });
+    }
   }
 })();
